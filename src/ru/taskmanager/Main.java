dbada197cc3c3384ff9ee06589f4bfcafb585883
a.gorinenko <a.gorinenko@itransition.com>
2017-07-20 15:58:35 +0300

    temp

--- src/ru/taskmanager/Main.java
+++ src/ru/taskmanager/Main.java
@@ -3,24 +3,21 @@ package ru.taskmanager;
 import ru.taskmanager.args.ParamsManager;
 import ru.taskmanager.commands.CommandResult;
 import ru.taskmanager.commands.Executor;
-import ru.taskmanager.config.ConfigurationManager;
-import ru.taskmanager.config.Settings;
-import ru.taskmanager.errors.ConfigurationException;
-import ru.taskmanager.utils.StringUtils;
 
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            ParamsManager manager = new ParamsManager(new String[]{ "HelpCommand" });
+            ParamsManager manager = new ParamsManager(args);
+
             Executor executor = new Executor(manager);
             List<CommandResult> result = executor.execute();
             String message = result.get(0).getMessage();
 
             System.out.print(message);
+            System.out.print(System.lineSeparator());
         } catch (Exception e) {
             e.printStackTrace();
         }
